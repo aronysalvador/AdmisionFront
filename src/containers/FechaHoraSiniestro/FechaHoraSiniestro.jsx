fc432d1cc3936e9cb632a2cee8aba1b32a85bb6c
Tony <tony.krikorian@outlook.com>
2020-07-14 15:13:00 -0400

    Integrar pantalla

--- src/containers/FechaHoraSiniestro/FechaHoraSiniestro.jsx
+++ src/containers/FechaHoraSiniestro/FechaHoraSiniestro.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { Fragment } from "react";
 import FechaSiniestro from "../../components/FechaSiniestro/FechaSiniestro";
 import HoraSiniestro from "./../../components/HoraSiniestro/HoraSiniestro";
-
+import { Button } from "@material-ui/core";
+import { getComunStyle } from "../../css/comun";
 const FechaHoraSiniestro = () => {
-  return <HoraSiniestro />;
+  const { buttonAchs } = getComunStyle();
+  return (
+    <Fragment>
+      <FechaSiniestro />
+      <HoraSiniestro />
+      <Button className={buttonAchs}>Siguiente</Button>
+    </Fragment>
+  );
 };
 
 export default FechaHoraSiniestro;
