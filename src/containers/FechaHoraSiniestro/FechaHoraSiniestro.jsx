05a405f006e1d5b90abcfee45c6bf3536b257f5d
Tony <tony.krikorian@outlook.com>
2020-07-14 15:44:57 -0400

    Integrar cabecera

--- src/containers/FechaHoraSiniestro/FechaHoraSiniestro.jsx
+++ src/containers/FechaHoraSiniestro/FechaHoraSiniestro.jsx
@@ -1,13 +1,33 @@
 import React, { Fragment } from "react";
 import FechaSiniestro from "../../components/FechaSiniestro/FechaSiniestro";
 import HoraSiniestro from "./../../components/HoraSiniestro/HoraSiniestro";
-import { Button } from "@material-ui/core";
+import { Button, Typography } from "@material-ui/core";
 import { getComunStyle } from "../../css/comun";
+import Cabecera from "../../components/cabecera/index";
+import {
+  useDispatch as dispatch,
+  useSelector,
+  shallowEqual,
+} from "react-redux";
+import { handleSetStep } from "../../redux/actions/AdmissionAction";
 
 const FechaHoraSiniestro = () => {
-  const { buttonAchs, root } = getComunStyle();
+  const { buttonAchs, root, pregunta } = getComunStyle();
+
+  const { step, percentage } = useSelector(
+    (state) => state.addmissionForm,
+    shallowEqual
+  );
+
   return (
     <div className={root}>
+      <Cabecera
+        dispatch={() => dispatch(handleSetStep(--step))}
+        percentage={percentage}
+      />
+      <Typography className={pregunta}>
+        Escribe la fecha y hora del accidente
+      </Typography>
       <FechaSiniestro />
       <HoraSiniestro />
       <Button className={buttonAchs}>Siguiente</Button>
