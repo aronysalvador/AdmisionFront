b579a86295c4ec599c12e343850800fab0e298f6
Tony <tony.krikorian@outlook.com>
2020-07-14 13:06:43 -0400

    Maquetear componente hora

--- src/components/HoraSiniestro/HoraSiniestro.jsx
+++ src/components/HoraSiniestro/HoraSiniestro.jsx
@@ -1,10 +1,54 @@
 import React, { useState, useEffect } from "react";
-
+import { Grid, Button } from "@material-ui/core";
+import { KeyboardArrowDown, KeyboardArrowUp } from "@material-ui/icons";
 const HoraSiniestro = () => {
   return (
-    <div>
-      <h1>Hora siniestro</h1>
-    </div>
+    <Grid container direction="row" justify="center" alignItems="center">
+      <Grid
+        item
+        style={{
+          borderColor: "black",
+          borderStyle: "solid",
+          borderSpacing: "2px",
+        }}
+      >
+        <div>
+          <Button variant="text">
+            <KeyboardArrowUp />
+          </Button>
+        </div>
+
+        <div style={{ textAlign: "center" }}>12</div>
+
+        <div>
+          <Button variant="text">
+            <KeyboardArrowDown />
+          </Button>
+        </div>
+      </Grid>
+      <Grid
+        item
+        style={{
+          borderColor: "black",
+          borderStyle: "solid",
+          borderSpacing: "2px",
+        }}
+      >
+        <div>
+          <Button variant="text">
+            <KeyboardArrowUp />
+          </Button>
+        </div>
+
+        <div style={{ textAlign: "center" }}>30</div>
+
+        <div>
+          <Button variant="text">
+            <KeyboardArrowDown />
+          </Button>
+        </div>
+      </Grid>
+    </Grid>
   );
 };
 
