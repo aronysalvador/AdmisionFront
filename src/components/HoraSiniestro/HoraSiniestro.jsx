341b1f16694824390c2654049663954d4b4dadd8
Tony <tony.krikorian@outlook.com>
2020-07-14 13:18:23 -0400

    Implementar logica minutos segundos

--- src/components/HoraSiniestro/HoraSiniestro.jsx
+++ src/components/HoraSiniestro/HoraSiniestro.jsx
@@ -1,7 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { Grid, Button } from "@material-ui/core";
 import { KeyboardArrowDown, KeyboardArrowUp } from "@material-ui/icons";
+
 const HoraSiniestro = () => {
+  const [horas, setHoras] = useState(() => {
+    return new Date().getHours();
+  });
+  const [minutos, setMinutos] = useState(() => {
+    return new Date().getMinutes();
+  });
+
   return (
     <Grid container direction="row" justify="center" alignItems="center">
       <Grid
@@ -12,16 +20,27 @@ const HoraSiniestro = () => {
           borderSpacing: "2px",
         }}
       >
+        <div style={{ textAlign: "center" }}>hora</div>
         <div>
-          <Button variant="text">
+          <Button
+            variant="text"
+            onClick={() => {
+              setHoras((h) => ++h);
+            }}
+          >
             <KeyboardArrowUp />
           </Button>
         </div>
 
-        <div style={{ textAlign: "center" }}>12</div>
+        <div style={{ textAlign: "center" }}>{horas}</div>
 
         <div>
-          <Button variant="text">
+          <Button
+            variant="text"
+            onClick={() => {
+              setHoras((h) => --h);
+            }}
+          >
             <KeyboardArrowDown />
           </Button>
         </div>
@@ -34,16 +53,27 @@ const HoraSiniestro = () => {
           borderSpacing: "2px",
         }}
       >
+        <div style={{ textAlign: "center" }}>minutos</div>
         <div>
-          <Button variant="text">
+          <Button
+            variant="text"
+            onClick={() => {
+              setMinutos((m) => ++m);
+            }}
+          >
             <KeyboardArrowUp />
           </Button>
         </div>
 
-        <div style={{ textAlign: "center" }}>30</div>
+        <div style={{ textAlign: "center" }}>{minutos}</div>
 
         <div>
-          <Button variant="text">
+          <Button
+            variant="text"
+            onClick={() => {
+              setMinutos((m) => --m);
+            }}
+          >
             <KeyboardArrowDown />
           </Button>
         </div>
