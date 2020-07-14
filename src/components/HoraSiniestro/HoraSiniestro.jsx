a773a7138861fed8e10e49c17409448fefc43802
Tony <tony.krikorian@outlook.com>
2020-07-14 12:24:29 -0400

    Add HoraSiniestro Component

---
+++ src/components/HoraSiniestro/HoraSiniestro.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+const HoraSiniestro = () => {
+  return (
+    <div>
+      <h1>Hora siniestro</h1>
+    </div>
+  );
+};
+
+export default HoraSiniestro;
