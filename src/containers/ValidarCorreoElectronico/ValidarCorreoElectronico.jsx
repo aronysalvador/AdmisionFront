2cef3a223bcf7b71a26ef25f3d30fea13bd28c89
Tony <tony.krikorian@outlook.com>
2020-07-17 14:57:51 -0400

    Crear container ValidarCorreoElectronico

---
+++ src/containers/ValidarCorreoElectronico/ValidarCorreoElectronico.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+const ValidarCorreoElectronico = () => {
+  return (
+    <div>
+      <h1>Validar Email</h1>
+    </div>
+  );
+};
+
+export default ValidarCorreoElectronico;
