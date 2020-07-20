ba9302c1f7ee920dec858bab522ee628bc6f42e5
Tony <tony.krikorian@outlook.com>
2020-07-20 10:20:10 -0400

    quitar codigo no comentado

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import reducer from "./redux/reducers";
 import { createStore, compose } from "redux";
 import { Provider } from "react-redux";
 import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
 import { PersistGate } from "redux-persist/integration/react";
 import storageSession from "redux-persist/lib/storage/session";
 
