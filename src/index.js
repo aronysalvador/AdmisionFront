023aee98fc5d07f40fcc30ddbb3c3fc8524405d1
Tony <tony.krikorian@outlook.com>
2020-07-20 10:17:20 -0400

    integrar redux-persistence

--- src/index.js
+++ src/index.js
@@ -6,11 +6,18 @@ import middleware from "./redux/middleware";
 import reducer from "./redux/reducers";
 import { createStore, compose } from "redux";
 import { Provider } from "react-redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
+import storageSession from "redux-persist/lib/storage/session";
 
-// const store = createStore(reducer, middleware);
-
+const persistConfig = {
+  key: "root",
+  storage: storageSession,
+};
+const persistedReducer = persistReducer(persistConfig, reducer);
 const store = createStore(
-  reducer,
+  persistedReducer,
   compose(
     middleware,
     typeof window === "object" &&
@@ -19,11 +26,13 @@ const store = createStore(
       : (f) => f
   )
 );
-
+let persistor = persistStore(store);
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store} pers>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
