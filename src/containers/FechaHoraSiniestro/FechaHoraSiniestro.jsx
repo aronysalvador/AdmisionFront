ca890871c42cb61107c1528f5c08e4819cbb14ee
Gonzalo Bustamante B <mercocha13@hotmail.com>
2020-07-20 12:54:15 -0400

    css historia 29165

--- src/containers/FechaHoraSiniestro/FechaHoraSiniestro.jsx
+++ src/containers/FechaHoraSiniestro/FechaHoraSiniestro.jsx
@@ -6,8 +6,11 @@ import { getComunStyle } from "../../css/comun";
 import Cabecera from "../../components/cabecera/index";
 import { useDispatch, useSelector, shallowEqual } from "react-redux";
 import { handleSetStep, updateForm } from "../../redux/actions/AdmissionAction";
+import {getSpaceStyle} from '../../css/spaceStyle'
 
 const FechaHoraSiniestro = () => {
+  const spaceStyle = getSpaceStyle();
+
   const [fechaSiniestro, setFechaSiniestro] = useState({});
   const [horaSiniestro, setHoraSiniestro] = useState({});
   const { buttonAchs, root, pregunta } = getComunStyle();
@@ -33,8 +36,11 @@ const FechaHoraSiniestro = () => {
       <Typography className={pregunta}>
         Escribe la fecha y hora del accidente
       </Typography>
+      <div  className={spaceStyle.space2} />
       <FechaSiniestro onChange={setFechaValueSiniestro} />
+      <div  className={spaceStyle.space1} />
       <HoraSiniestro onChange={setHoraValueSiniestro} />
+      <div className={spaceStyle.space2} />
       <Button
         className={buttonAchs}
         onClick={() => {
