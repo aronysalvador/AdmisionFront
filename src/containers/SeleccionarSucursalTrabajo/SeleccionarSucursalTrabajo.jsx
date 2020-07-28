ac0308637e06231187946e4bbd75f5b08d6b8fdc
Tony <tony.krikorian@outlook.com>
2020-07-28 09:24:04 -0400

    Creación componente Seleccionar comuna

---
+++ src/containers/SeleccionarSucursalTrabajo/SeleccionarSucursalTrabajo.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+const SeleccionarSucursalTrabajo = () => {
+  return (
+    <div>
+      <h1>Selecciona tu comuna de trabajo</h1>
+    </div>
+  );
+};
+
+export default SeleccionarSucursalTrabajo;
