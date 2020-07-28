d054d7bedd41e27b7838f7c2dbe601cd5d7d2611
Tony <tony.krikorian@outlook.com>
2020-07-28 09:46:38 -0400

    Button custom

---
+++ src/components/BotonSeleccionarCustom/BotonSeleccionarCustom.jsx
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+
+const BotonSeleccionarCustom = ({ nombreSucursal, comunaNombre }) => {
+  return <div></div>;
+};
+
+export default BotonSeleccionarCustom;
