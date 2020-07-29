46a728b62a202586f3f8dc12c910938ab458ffec
Tony <tony.krikorian@outlook.com>
2020-07-29 08:37:39 -0400

    Quitar codigo innecesario

--- src/containers/SeleccionarSucursalTrabajo/EditarSucursal.jsx
+++ src/containers/SeleccionarSucursalTrabajo/EditarSucursal.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector, shallowEqual } from "react-redux";
 import { sucursalesOficina } from "../../util/fakeApi";
 import SeleccionarSucursalTrabajo from "./SeleccionarSucursalTrabajo";
 import SeleccionarComuna from "./SeleccionarComuna";
@@ -7,10 +6,6 @@ import SeleccionarComuna from "./SeleccionarComuna";
 const EditarSucursal = () => {
   const [cantidadSucursales] = useState(() => sucursalesOficina.length);
 
-  const { step, percentage } = useSelector(
-    (state) => state.addmissionForm,
-    shallowEqual
-  );
   if (cantidadSucursales <= 6)
     return <SeleccionarSucursalTrabajo sucursalesEmpresa={sucursalesOficina} />;
   if (cantidadSucursales >= 6)
