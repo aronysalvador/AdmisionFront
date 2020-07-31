142221ee15788d0c7e0e83941730d108ac51daf8
Tony <tony.krikorian@outlook.com>
2020-07-31 13:33:30 -0400

    Crear pantalla remuneraciones

---
+++ src/containers/Remuneracion/Remuneracion.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import Cabecera from "../../components/cabecera/index";
+import { useDispatch, useSelector, shallowEqual } from "react-redux";
+import { handleSetStep, updateForm } from "../../redux/actions/AdmissionAction";
+import { getSpaceStyle } from "../../css/spaceStyle";
+import { getComunStyle } from "../../css/comun";
+import { Typography } from "@material-ui/core";
+
+const Remuneracion = () => {
+  const dispatch = useDispatch();
+  const { root, pregunta, bottomElement } = getComunStyle();
+  const spaceStyle = getSpaceStyle();
+
+  const { step, percentage, sucursales } = useSelector(
+    (state) => state.addmissionForm,
+    shallowEqual
+  );
+  return (
+    <div className={root}>
+      <Cabecera
+        dispatch={() => dispatch(handleSetStep(step - 1))}
+        percentage={percentage}
+      />
+      <Typography className={pregunta}>
+        Selecciona la sucursal en donde trabajas
+      </Typography>
+    </div>
+  );
+};
+
+export default Remuneracion;
