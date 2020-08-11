61c7504039efa6ea512ba73a0ea283b1585de1ab
Tony <tony.krikorian@outlook.com>
2020-08-11 10:53:51 -0400

    Add componente Caja RUT Siniestro ID

---
+++ src/containers/PantallaFinal/CajaRutSiniestro.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const CajaRutSiniestro = ({ textoPrincipal, textoSecundario }) => {
+  return (
+    <div
+      style={{
+        width: "47%",
+        border: "1px solid #787878",
+        borderRadius: "10px",
+        padding: "10px",
+        height: "60px",
+        paddingBottom: "5px",
+        backgroundColor: "#FFFFFF",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          alignItems: "left",
+          flexDirection: "column",
+        }}
+      >
+        <span
+          style={{ color: "#373737", fontWeight: "bold", fontSize: "17px" }}
+        >
+          {textoPrincipal}
+        </span>
+        <span
+          style={{ color: "#787878", fontWeight: "bold", fontSize: "13px" }}
+        >
+          {textoSecundario}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default CajaRutSiniestro;
