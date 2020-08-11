07a1d78ceec5f279254ec6d2a9fd1bb83e6b56bb
Tony <tony.krikorian@outlook.com>
2020-08-11 11:23:58 -0400

    Renderizar datos

--- src/containers/PantallaFinal/IndiciacionesPaciente.jsx
+++ src/containers/PantallaFinal/IndiciacionesPaciente.jsx
@@ -1,13 +1,35 @@
 import React from "react";
 import { getWelcomeStyle } from "../../css/welcomeStyle";
-import { getComunStyle } from "../../css/comun";
-import { getSpaceStyle } from "../../css/spaceStyle";
+import Typography from "@material-ui/core/Typography";
 
-const IndiciacionesPacientes = () => {
+const IndiciacionesPacientes = ({ data }) => {
   const welcomeStyle = getWelcomeStyle();
-  const comunStyle = getComunStyle();
-  const spaceStyle = getSpaceStyle();
-  return <div></div>;
+
+  return (
+    <div className={welcomeStyle.titleContainerCards}>
+      {data.map(({ icono, textoPrimario, textoSecundario }) => (
+        <div className={welcomeStyle.divRowBottom}>
+          <img src={`./static/${icono}`} alt="" />
+          <div className={welcomeStyle.itemBegin}>
+            <Typography
+              variant="p"
+              component="p"
+              className={welcomeStyle.itemText}
+            >
+              {textoPrimario}
+            </Typography>
+            <Typography
+              variant="p"
+              component="p"
+              className={welcomeStyle.pBegin}
+            >
+              {textoSecundario}
+            </Typography>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default IndiciacionesPacientes;
