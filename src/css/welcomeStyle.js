b23bd740940abfc22470baa1ffa3d3171833dd39
unknown <onunez@achs.cl>
2020-08-11 10:12:18 -0400

    pantalla-empecemos

--- src/css/welcomeStyle.js
+++ src/css/welcomeStyle.js
@@ -27,7 +27,11 @@ export const getWelcomeStyle = makeStyles((theme) => ({
         height: '5em',
         verticalAlign: 'middle',
     },
-
+    avatarBegin: {
+        width: '3.5em',
+        height: '3.5em',
+        verticalAlign: 'middle',
+    },
     button: {
         width: '100%',
         height: '4em',
@@ -72,28 +76,44 @@ export const getWelcomeStyle = makeStyles((theme) => ({
     },
 
     beginContainer:{
-        padding: '1.5em',
+        padding: '1em 1em 0 1em',
+    },
+    beginContainerCard:{
+        padding: '0.5em 1em 0 1em',
     },
     TextContainer:{
         padding: '1.5em 1.5em 0 1.5em',
-        marginTop:'30px'
+        marginTop:'2px'
     },
     titleContainer:{
-        padding: '1em',
+        padding: '0.5em 1.5em 0.5em 1.5em',
         background: '#FFFFFF',
         boxShadow: '2px 2px 10px rgba(203, 203, 203, 0.4)',
         borderRadius: '10px',
-
+    },
+    titleContainerCards:{
+        padding: '1.5em',
+        background: '#FFFFFF',
+        boxShadow: '2px 2px 10px rgba(203, 203, 203, 0.4)',
+        borderRadius: '10px',
+    },
+    divRow: {        
         display: 'flex',
+        // justifyContent: 'center',
         alignItems: 'center',
-        justifyContent: 'center',
+    },
+    divRowBottom: {        
+        display: 'flex',
+        // justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: '20px'
     },
 
     txtBegin:{
         fontFamily: 'Catamaran',
         fontStyle: 'normal',
         fontWeight: 'bold',
-        fontSize: '32px',
+        fontSize: '25px',
         lineHeight: '34px',
         color: '#007A33'
     },
@@ -105,7 +125,39 @@ export const getWelcomeStyle = makeStyles((theme) => ({
         fontSize: '16px',
         lineHeight: '20px',
         color: '#000000',
-        marginLeft: '10px'
+        marginLeft: '20px'
+    },
+    subTitleBegin:{
+        fontFamily: 'Catamaran',
+        fontStyle: 'normal',
+        fontWeight: 'bold',
+        fontSize: '16px',
+        lineHeight: '18px',
+        color: '#373737',
+        marginBottom: '10px'
+    },
+    itemBegin:{
+        marginLeft: '20px',
+    },
+    itemText:{
+        fontFamily: 'Catamaran',
+        fontStyle: 'normal',
+        fontWeight: 'bold',
+        fontSize: '16px',
+        lineHeight: '20px',
+        color: '#000000',
+    },
+    pBegin:{
+        fontFamily: 'Catamaran',
+        fontStyle: 'normal',
+        fontWeight: 'normal',
+        fontSize: '13px',
+        lineHeight: '16px',
+        color: '#000000',
+        maxWidth: '210px'
+    },
+    bottomBegin:{
+        marginTop: '20px'
     }
 
 }))
