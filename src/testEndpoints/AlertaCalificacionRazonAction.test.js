f1cccc0b80ca0a5aebba5978ddddf1425d52d922
Arony Salvador <asalvadorn@achs.com>
2020-08-13 23:38:09 -0400

    test EndPoint Alertas

---
+++ src/testEndpoints/AlertaCalificacionRazonAction.test.js
@@ -0,0 +1,16 @@
+import { getData } from "./../redux/actions/AlertaCalificacionRazonAction";
+
+describe("Alertas", () => {
+  it("Probar Endpoint que obtiene las Alertas", async () => {
+    const resultado = await getData();
+    const status = resultado ? resultado.status : 0;
+    if (status == 200) {
+      const total = resultado ? resultado.data.opciones.length : 0;
+      expect(total).toBeGreaterThan(0);
+      console.log("Alertas: " + total);
+    } else {
+      console.log("status: " + status);
+    }
+    expect(resultado.status).toEqual(200);
+  });
+});
