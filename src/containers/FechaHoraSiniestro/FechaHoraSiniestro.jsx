72e545c278b4daeafc4b743ff51cef4df6a2fb57
Arony Salvador <asalvadorn@achs.cl>
2020-08-20 10:15:09 -0400

    Cambio de Back

--- src/containers/FechaHoraSiniestro/FechaHoraSiniestro.jsx
+++ src/containers/FechaHoraSiniestro/FechaHoraSiniestro.jsx
@@ -37,7 +37,7 @@ const FechaHoraSiniestro = () => {
   return (
     <div className={root}>
       <Cabecera
-        dispatch={() => dispatch(handleSetStep(8))}
+        dispatch={() => dispatch(handleSetStep(9))}
         percentage={percentage}
       />
       <Typography className={pregunta}>
