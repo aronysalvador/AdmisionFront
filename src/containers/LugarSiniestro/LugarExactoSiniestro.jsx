6d73ad73ea7b6e13d34a26fc431d5e5ff6346858
Orianna Nuñez <onunez@achs.cl>
2020-09-23 11:33:33 -0300

    cambio-comuna-direccion

--- src/containers/LugarSiniestro/LugarExactoSiniestro.jsx
+++ src/containers/LugarSiniestro/LugarExactoSiniestro.jsx
@@ -4,9 +4,8 @@ import { Button, Typography } from "@material-ui/core"
 import Cabecera from "../../components/cabecera/index"
 import { useSelector, shallowEqual, useDispatch } from "react-redux"
 import { handleSetStep, updateForm } from "../../redux/actions/AdmissionAction"
-import { getData } from "../../redux/actions/ComunaAction"
 import DireccionGeo from '../../components/share/DireccionGeo'
-import { eliminarDiacriticos } from './../../helpers/utils'
+import { validarDireccion } from './../../helpers/utils'
 
 const LugarExactoSiniestro = () => {
   const {
@@ -51,46 +50,13 @@ const LugarExactoSiniestro = () => {
     }
   },[sucursal])
 
+  
   const validaDireccion = async()=>{
-    setNombreComuna("")
-      console.log(sucursal)
-      if(typeof sucursal.description === 'string'){
-        const fragmentos  = sucursal.description.split(",")
-        console.log(fragmentos)
-          if(Array.isArray(fragmentos) && fragmentos.length >= 3 && fragmentos[0].match(/\d+/g) ){
-            console.log("direccion contiene numero ....")
-            const result = await getData()
-              if(result.status === 200){
-                var COMUNAS = result.data.content[0]
-                  if(Array.isArray(COMUNAS)){           
-                        var comuna = fragmentos[fragmentos.length-2].toUpperCase().trim()                                             
-                        if(comuna.includes("REGIÓN")){
-                          comuna = fragmentos[1].toUpperCase().trim()
-                        }
-                        console.log("validando comuna: "+comuna) 
+    const resultado = await validarDireccion(sucursal)
+    setNombreComuna(resultado.comuna)
+    setValido(resultado.valida)
+ }
 
-                        var resultValid = COMUNAS.find(ele => eliminarDiacriticos(ele.nombre) === eliminarDiacriticos(comuna))
-                        console.log(resultValid) 
-                        if( resultValid !== undefined  ){
-                          console.log("comuna: "+comuna+" valida")
-                          setNombreComuna(comuna)
-                          setValido( true )
-                        }else{
-                          setValido( false )
-                        }
-                  }else{
-                    setValido( false )
-                  }
-              }else{
-                setValido( false )
-              }
-          }else{
-            setValido( false )
-          }
-      }else{
-        setValido( false )
-      }    
-  }
 
   return (
     <div className={root}>
