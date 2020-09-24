182c208e250d2826b0311f7b231e861223b8e8fe
GelenMR <gelen.montilla.r@gmail.com>
2020-09-24 09:21:31 -0300

    box testigo responsable

--- src/components/share/BoxACHS/index.jsx
+++ src/components/share/BoxACHS/index.jsx
@@ -6,7 +6,7 @@ import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 import { cardSiniestroStyles } from "../../../css/cardSiniestroStyle";
 
 const BoxACHS = (props) => {
-    const { titulo, contenido,step } = props
+    const { titulo, contenido, step } = props
     const classes = cardSiniestroStyles();
     const dispatch = useDispatch();
     return (<div className={classes.container}>
@@ -14,7 +14,6 @@ const BoxACHS = (props) => {
                     <div>
                         <Typography color="textSecondary" gutterBottom className={classes.itemId}>
                             {titulo}
-                            {/* <EditIcon style={{cursor : "pointer"}} onClick={() => dispatch(handleSetStep(step))} /> */}
                         </Typography>
                     </div>
                     <div>
