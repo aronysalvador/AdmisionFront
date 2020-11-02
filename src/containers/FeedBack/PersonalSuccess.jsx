0e4bab4d46641cd75958601ffd497d43871e7795
GelenMR <gelen.montilla.r@gmail.com>
2020-11-02 14:15:15 -0300

    estilos desktop en pantallas AFP - Isapre - RelatoSuccess

--- src/containers/FeedBack/PersonalSuccess.jsx
+++ src/containers/FeedBack/PersonalSuccess.jsx
@@ -7,9 +7,7 @@ import { handleSetStep } from "../../redux/actions/AdmissionAction";
 import Indiciaciones from "../../components/Indicaciones";
 import Cabecera from "../../components/cabecera/cabeceraSinBarra";
 import Header from "../../components/header/index";
-
 import { Grid } from '@material-ui/core';
-
 import { getWelcomeStyle } from "../../css/welcomeStyle";
 import { getComunStyle } from "../../css/comun";
 import { getSpaceStyle } from "../../css/spaceStyle";
@@ -22,127 +20,128 @@ const PersonalSuccess = (props) =>{
     const spaceStyle = getSpaceStyle();
 console.log(step);
     return(
-        <div className={comunStyle.rootBegin}>
-            <div className={comunStyle.displayDesk}> 
-                <Header
-                    userMsal={ microsoftReducer.userMsal }
-                    // step={1}
-                />
-            </div>
-            <div className={welcomeStyle.backgroundBoxAchsDesk}>
-                <div className={welcomeStyle.beginContainer}>
-                    <div className={comunStyle.displayMobile}>
-                        <Cabecera 
-                            dispatch={() => dispatch(handleSetStep(5.1))} 
-                            color={"#373737" }  
-                            percentage={-1} 
-                            noSpace={true} />
-                    </div>
-                    <div className={comunStyle.displayDesk}>
-                        <Cabecera 
-                            dispatch={() => dispatch(handleSetStep(5.1))} 
-                            color={"#fff" } 
-                            percentage={-1} 
-                            noSpace={true} />
-                    </div>
-                    <div className={comunStyle.displayMobile}>
-                        <div className={welcomeStyle.avatarContainerRight}>
-                            <Avatar className={welcomeStyle.avatar}>{microsoftReducer.userMsal.iniciales}</Avatar>
-                        </div>
-                        <div className={spaceStyle.space6} />
-                    </div>
-                    <div className={comunStyle.titleDesk}>
-                        <div className={welcomeStyle.TextContainer}>
-                            <div className={comunStyle.displayMobile}>
-                                <img alt="Excelente" src="static/icon-check.png" className={welcomeStyle.iconCircular} />
-                            </div>    
-                            <Typography
-                            variant="h1"
-                            component="h1"
-                            className={welcomeStyle.titleBegin}
-                            >
-                                ¡Excelente!
-                            </Typography>
-                            <div style={{display: 'flex'}}>
-                                <Typography
-                                variant="p"
-                                component="p"
-                                className={[welcomeStyle.titleBegin2, welcomeStyle.subtitleBegin]}
-                                >
-                                    Usuario Identificado&nbsp;
-                                </Typography>
-                                <div className={comunStyle.displayDeskInline}>
-                                    <img alt="Excelente" src="static/icon-check.png" className={welcomeStyle.iconCircular} />
-                                </div>
-                            </div>
-                            
-                        </div>
-                        
-                        <div className={comunStyle.displayDeskInline}>
-                            <Grid component="span" className={comunStyle.imgPrimaryDesk}>
-                                <img alt="excelente" src="static/excelent.svg" />
-                            </Grid>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className={welcomeStyle.beginContainer}>
-                <div className={comunStyle.displayDesk}> 
-                    <div className={spaceStyle.space2} />   
-                </div>
-                <div className={comunStyle.textCenterDesk}>
-                    <Typography
-                        variant="h5"
-                        component="h5"
-                        className={welcomeStyle.subTitleBegin}
-                        >
-                        Ahora capturarás el relato:
-                    </Typography>
-                    <div className={comunStyle.displayDesk}>
-                        <div className={spaceStyle.space2} />
-                    </div>
-                    <div className={comunStyle.boxDesk}>
-                        <Indiciaciones
-                            indicaciones={[
-                                {
-                                    icono: "espera.svg",
-                                    textoPrimario: "Completa las frases",
-                                    textoSecundario: "mientras escuchas con atención",
-                                    clase: welcomeStyle.divRowBottom,
-                                },
-                                {
-                                    icono: "sms.svg",
-                                    textoPrimario: "Transcribe con fidelidad",
-                                    textoSecundario: "usando los tiempos verbales sugeridos",
-                                    clase: welcomeStyle.divRowBottom,
-                                },
-                                {
-                                    icono: "work.svg",
-                                    textoPrimario: "Mantén la autenticidad",
-                                    textoSecundario: "sin alterar los sucesos",
-                                    clase: welcomeStyle.divRow,
-                                },
-                            ]}
-                        />
-                        <div className={comunStyle.displayMobile}>
-                            <div className={spaceStyle.space2} />
-                        </div>
-                        <div className={welcomeStyle.bottomBegin}>
-                            <Button
-                                className={comunStyle.buttonAchs}
-                                variant="contained"
-                                onClick={() => dispatch(handleSetStep(6))}
-                            >
-                                Capturar el relato
-                            </Button>
-                        </div>
-                    </div>
-                </div>
-            </div> 
-            <div className={comunStyle.displayDesk}>
-                <div className={spaceStyle.space2} />
-            </div>           
+      <div className={comunStyle.rootBegin}>
+        <div className={comunStyle.displayDesk}> 
+          <Header
+            userMsal={ microsoftReducer.userMsal }
+            // step={1}
+          />
         </div>
+        <div className={welcomeStyle.backgroundBoxAchsDesk}>
+          <div className={welcomeStyle.beginContainer}>
+            <div className={comunStyle.displayMobile}>
+              <Cabecera 
+                dispatch={() => dispatch(handleSetStep(5.1))} 
+                color={"#373737" }  
+                percentage={-1} 
+                noSpace={true} />
+            </div>
+            <div className={comunStyle.displayDesk}>
+              <Cabecera 
+                dispatch={() => dispatch(handleSetStep(5.1))} 
+                color={"#fff" } 
+                percentage={-1} 
+                noSpace={true} />
+            </div>
+            <div className={comunStyle.displayMobile}>
+              <div className={welcomeStyle.avatarContainerRight}>
+                <Avatar className={welcomeStyle.avatar}>
+                  {microsoftReducer.userMsal.iniciales}
+                </Avatar>
+              </div>
+              <div className={spaceStyle.space6} />
+            </div>
+            <div className={comunStyle.titleDesk}>
+              <div className={welcomeStyle.TextContainer}>
+                <div className={comunStyle.displayMobile}>
+                  <img alt="Excelente" src="static/icon-check.png" className={welcomeStyle.iconCircular} />
+                </div>    
+                <Typography
+                  variant="h1"
+                  component="h1"
+                  className={welcomeStyle.titleBegin}
+                >
+                  ¡Excelente!
+                </Typography>
+                <div style={{display: 'flex'}}>
+                  <Typography
+                  variant="p"
+                  component="p"
+                  className={[welcomeStyle.titleBegin2, welcomeStyle.subtitleBegin]}
+                  >
+                    Usuario Identificado&nbsp;
+                  </Typography>
+                  <div className={comunStyle.displayDeskInline}>
+                    <img alt="Excelente" src="static/icon-check.png" className={welcomeStyle.iconCircular} />
+                  </div>
+                </div>
+              </div>
+                
+              <div className={comunStyle.displayDeskInline}>
+                <Grid component="span" className={comunStyle.imgPrimaryDesk}>
+                  <img alt="excelente" src="static/excelent.svg" />
+                </Grid>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={welcomeStyle.beginContainer}>
+          <div className={comunStyle.displayDesk}> 
+            <div className={spaceStyle.space2} />   
+          </div>
+          <div className={comunStyle.textCenterDesk}>
+            <Typography
+              variant="h5"
+              component="h5"
+              className={welcomeStyle.subTitleBegin}
+              >
+              Ahora capturarás el relato:
+            </Typography>
+            <div className={comunStyle.displayDesk}>
+              <div className={spaceStyle.space2} />
+            </div>
+            <div className={comunStyle.boxDesk}>
+              <Indiciaciones
+                indicaciones={[
+                  {
+                    icono: "espera.svg",
+                    textoPrimario: "Completa las frases",
+                    textoSecundario: "mientras escuchas con atención",
+                    clase: welcomeStyle.divRowBottom,
+                  },
+                  {
+                    icono: "sms.svg",
+                    textoPrimario: "Transcribe con fidelidad",
+                    textoSecundario: "usando los tiempos verbales sugeridos",
+                    clase: welcomeStyle.divRowBottom,
+                  },
+                  {
+                    icono: "work.svg",
+                    textoPrimario: "Mantén la autenticidad",
+                    textoSecundario: "sin alterar los sucesos",
+                    clase: welcomeStyle.divRow,
+                  },
+                ]}
+              />
+              <div className={comunStyle.displayMobile}>
+                <div className={spaceStyle.space2} />
+              </div>
+              <div className={welcomeStyle.bottomBegin}>
+                <Button
+                  className={comunStyle.buttonAchs}
+                  variant="contained"
+                  onClick={() => dispatch(handleSetStep(6))}
+                >
+                  Capturar el relato
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div> 
+        <div className={comunStyle.displayDesk}>
+          <div className={spaceStyle.space2} />
+        </div>           
+      </div>
     )
 }
 
