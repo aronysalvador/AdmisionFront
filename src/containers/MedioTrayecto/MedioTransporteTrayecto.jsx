90bf9a1109b99a09510f0d71e067c97423b392b5
GelenMR <gelen.montilla.r@gmail.com>
2020-11-30 16:00:19 -0300

    cambiando estructura de data documento trayecto

--- src/containers/MedioTrayecto/MedioTransporteTrayecto.jsx
+++ src/containers/MedioTrayecto/MedioTransporteTrayecto.jsx
@@ -10,6 +10,7 @@ import Grid from '@material-ui/core/Grid';
 import Header from "../../components/header/index";
 import AutoComplete from "@material-ui/lab/Autocomplete";
 import { getMediosTransporteTrayecto } from "../../redux/actions/TrayectoAction";
+import relato from './../../img/relato.svg';
 
 const MedioTransporteTrayecto = () => {
   let {
@@ -69,7 +70,7 @@ console.log(sugerenciasMedios);
         </Grid>
         <div className={comunClass.displayDeskImg}>
           <Grid component="span" className={comunClass.imgPrimaryDesk}>
-            <img alt="identify" src="static/relato.svg" className={comunClass.imgPrimaryWidth} />
+            <img alt="relato" src={relato} className={comunClass.imgPrimaryWidth} />
           </Grid>
         </div>
       </div>
@@ -121,7 +122,9 @@ console.log(sugerenciasMedios);
             className={comunClass.buttonAchs}
             variant="contained"
             onClick={() => {
-              dispatch(updateForm("medioTransporteSiniestro", medioTransporte));
+              dispatch(updateForm("medioTransporteSiniestro", medioTransporte)); 
+              var respDoc = {tag:"MedioTransp", valor: {medioTransporte}};
+              dispatch(updateForm("CamposDocumentos", respDoc));
               dispatch(handleSetStep(6.03));
             }}
           >
