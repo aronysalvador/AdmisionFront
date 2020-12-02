b7fac27157c470f99d4b0d878d486ce7ffa4e03b
Sebastian Eduardo Cristi Castillo <secristic@ext.achs.cl>
2020-12-02 18:55:50 -0300

    se sigue intentando solucionar

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sh reemplaza-envs.sh > /usr/share/nginx/html/config.js
-cat "window.entorno=\"$ENTORNO\"" >> /usr/share/nginx/html/config.js
+echo "window.entorno=\"$ENTORNO\"" >> /usr/share/nginx/html/config.js
 nginx -g "daemon off;"
