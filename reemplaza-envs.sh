3762664e71326eb6c5327b7a2d5873717f9c6323
Sebastian Eduardo Cristi Castillo <secristic@ext.achs.cl>
2020-12-02 17:19:17 -0300

    cambio del sh para intento de solucion del problema

--- reemplaza-envs.sh
+++ reemplaza-envs.sh
@@ -4,11 +4,9 @@ case "$ENTORNO" in
     qaTest) cat env.qa.config.js > src/setupTests.js && echo "Cambiando variables de entorno a $ENTORNO" ;;
     stagingTest) cat env.stg.config.js > src/setupTests.js && echo "Cambiando variables de entorno a $ENTORNO";;
     produccionTest) cat env.prod.config.js > src/setupTests.js && echo "Cambiando variables de entorno a $ENTORNO";;
-    
     desarrollo) cat env.desarrollo.config.js > /usr/share/nginx/html/config.js && echo "Cambiando variables de entorno a $ENTORNO" ;;
     qa) cat env.qa.config.js > /usr/share/nginx/html/config.js && echo "Cambiando variables de entorno a $ENTORNO" ;;
     staging) cat env.stg.config.js > /usr/share/nginx/html/config.js && echo "Cambiando variables de entorno a $ENTORNO";;
     produccion) cat env.prod.config.js > /usr/share/nginx/html/config.js && echo "Cambiando variables de entorno a $ENTORNO";;
-    
     *) echo "Entorno '$ENTORNO' no valido" ;;
 esac
