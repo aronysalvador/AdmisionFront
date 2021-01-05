94f6595047524a7232bc4785fac8b1734426fbfb
Arony Salvador <asalvadorn@achs.cl>
2021-01-05 10:02:12 -0300

    Modificacion Test CrearCaso

--- src/__tests__/CrearCaso.test.js
+++ src/__tests__/CrearCaso.test.js
@@ -3,10 +3,17 @@ import { sendingCaso } from  './../redux/actions/AdmissionAction';
 describe("Load",()=>{
   
     it("Probar Endpoint de creación de caso",async()=>{
-
-        const resultado = await sendingCaso({})
+        expect.assertions(2);
+        try {
+            const resultado = await sendingCaso({})
+        } catch (e) {
+            expect(e).toBeDefined()
+            expect(e.response.status).toEqual(409)
+        }
         
-        expect(resultado.data.status).toEqual(500)
+ 
+        
+        //expect(resultado.statusCode).toEqual(500)
     })
 })
 
