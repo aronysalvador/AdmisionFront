e90e14c2a6387491a2c147a7ed87fa365177499a
Sebastian Eduardo Cristi Castillo <secristic@ext.achs.cl>
2021-01-05 15:29:26 -0300

    Se realiza cambio para solucionar bug que envia cambio de pagina donde no deberia realizarlo

--- src/redux/actions/AdmissionAction.js
+++ src/redux/actions/AdmissionAction.js
@@ -445,7 +445,7 @@ export const saveAnswer = (answerType, answerValue, step) => {
 export function sendIsapres(id) {
     return (dispatch) => {
         dispatch(sendCallIsapres(id));
-        dispatch(handleSetStep(6));
+        // dispatch(handleSetStep(6));
     };
 }
 
