adf7b8fa58540460c1f8f47a0b4246d6adb761bb
Sebastian Eduardo Cristi Castillo <secristic@ext.achs.cl>
2021-01-07 15:15:16 -0300

    Se adaptan las pruebas unitarias para controlar correctamente el token segun tarea #43444

--- src/__tests__/SucursalesAction.test.js
+++ src/__tests__/SucursalesAction.test.js
@@ -1,8 +1,15 @@
 import { obtenerData } from "./../redux/actions/SucursalesAction";
+import { obtenerUsuarioSap } from "../redux/actions/microsoft.action"
+
+const getToken = async() => {
+  const result = await obtenerUsuarioSap("sadiazg@achs.cl")
+  return result.data.token
+}
+
 
 describe("Sucursales", () => {
   it("Probar Endpoint que obtiene data de Sucursales", async () => {
-    const resultado = await obtenerData("70360100-6");
+    const resultado = await obtenerData("70360100-6", await getToken());
 
     const status = resultado ? resultado.status : 0;
     if (status == 200) {
--- src/__tests__/UserCenter.test.js
+++ src/__tests__/UserCenter.test.js
@@ -1,9 +1,17 @@
 import { getCenters } from "./../redux/actions/microsoft.action";
 import { getData } from "./../redux/actions/UserCenterAction";
 
+import { obtenerUsuarioSap } from "../redux/actions/microsoft.action"
+
+const getToken = async() => {
+    const result = await obtenerUsuarioSap("sadiazg@achs.cl")
+    return result.data.token
+}
+
+
 describe("Obtener centros del usuario microsoft", () => {
   it("Probar Endpoint que obtiene centros", async () => {
-    const resultado = await getCenters("asalvadorn@ext.achs.cl");
+    const resultado = await getCenters("asalvadorn@ext.achs.cl", await getToken());
     const status = resultado ? resultado.status : 0;
     if (status == 200) {
       console.log("Centros: " + resultado.data.data.length);
@@ -16,7 +24,7 @@ describe("Obtener centros del usuario microsoft", () => {
 
 describe("Guardar centro del usuario microsoft", () => {
   it("Probar Endpoint que enviar centro del usuario", async () => {
-    const resultado = await getData("asalvadorn@ext.achs.cl",{"ID":50021418,"NOMBRE":"UNIDAD SALUD EP ANTOFAGASTA","tipo":"UC"}
+    const resultado = await getData("asalvadorn@ext.achs.cl",{"ID":50021418,"NOMBRE":"UNIDAD SALUD EP ANTOFAGASTA","tipo":"UC"}, await getToken()
 );
     const status = resultado ? resultado.status : 0;
     if (status == 200) {
