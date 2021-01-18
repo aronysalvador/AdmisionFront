8873b37cb4e2ec021d3c93ae76bd2e433716d846
GelenMR <gelen.montilla.r@gmail.com>
2021-01-18 17:31:33 -0300

    fix validacion centro admisionista

--- src/redux/actions/microsoft.action.js
+++ src/redux/actions/microsoft.action.js
@@ -1,8 +1,8 @@
 import {
-  MSAL_INIT,
-  MSAL_SUCCESS,
-  MSAL_FAILURE,
-  SAVE_TOKEN,
+    MSAL_INIT,
+    MSAL_SUCCESS,
+    MSAL_FAILURE,
+    SAVE_TOKEN,
 } from "../types/microsoftType.js";
 import msalservice from "../../services/msalservice";
 import graphservice from "../../services/graphservice";
@@ -10,114 +10,115 @@ import Axios from "axios";
 import { handleSetStep, updateForm } from "../../redux/actions/AdmissionAction";
 
 export async function getUserProfile(scopes) {
-  const accessToken = await msalservice.acquireTokenSilent({ scopes });
-  if (accessToken) {
-    const user = await graphservice(accessToken);
-    return user;
-  }
-  return null;
+    const accessToken = await msalservice.acquireTokenSilent({ scopes });
+    if (accessToken) {
+        const user = await graphservice(accessToken);
+        return user;
+    }
+    return null;
 }
 
-export const login = (scopes) => async (dispatch, getState) => {
-  dispatch({ type: MSAL_INIT });
+export const login = (scopes) => async(dispatch, getState) => {
+    dispatch({ type: MSAL_INIT });
 
-  try {
-    await msalservice.loginPopup({ scopes, prompt: "select_account" });
-    const user = await getUserProfile(scopes);
-    let accessToken = await msalservice.acquireTokenSilent({ scopes })
-    const userTempArray = user.displayName.split(",");
-    let iniciales = "";
-    if (Array.isArray(userTempArray) && userTempArray.length === 2) {
-      iniciales =
-        String(userTempArray[1]).trim().charAt(0) +
-        String(userTempArray[0]).trim().charAt(0);
-    }
-    const userMsal = {
-      displayName: user.displayName,
-      email: user.mail || user.userPrincipalName,
-      iniciales: iniciales,
-    };
-    dispatch({ type: MSAL_SUCCESS, payload: userMsal });
-    //PARCHE
-    var userSAP =await  homologacionSap(dispatch, userMsal.email, accessToken)
-    if(userSAP){
-      var step =await  isNuevaAdmisionista(dispatch, userMsal.email, getState)
-      dispatch(handleSetStep(step));
-    }else{
-      dispatch(handleSetStep(1003));
-    }
+    try {
+        await msalservice.loginPopup({ scopes, prompt: "select_account" });
+        const user = await getUserProfile(scopes);
+        let accessToken = await msalservice.acquireTokenSilent({ scopes })
+        const userTempArray = user.displayName.split(",");
+        let iniciales = "";
+        if (Array.isArray(userTempArray) && userTempArray.length === 2) {
+            iniciales =
+                String(userTempArray[1]).trim().charAt(0) +
+                String(userTempArray[0]).trim().charAt(0);
+        }
+        const userMsal = {
+            displayName: user.displayName,
+            email: user.mail || user.userPrincipalName,
+            iniciales: iniciales,
+        };
+        dispatch({ type: MSAL_SUCCESS, payload: userMsal });
+        //PARCHE
+        var userSAP = await homologacionSap(dispatch, userMsal.email, accessToken)
+        if (userSAP) {
+            var step = await isNuevaAdmisionista(dispatch, userMsal.email, getState)
+            dispatch(handleSetStep(step));
+        } else {
+            dispatch(handleSetStep(1003));
+        }
 
-    
-  } catch (err) {
-    let error = {};
-    if (typeof err === "string") {
-      const errParts = err.split("|");
-      error =
-        errParts.length > 1
-          ? { message: errParts[1], debug: errParts[0] }
-          : { message: err };
-    } else {
-      error = {
-        message: err.message,
-        debug: JSON.stringify(err),
-      };
+
+    } catch (err) {
+        let error = {};
+        if (typeof err === "string") {
+            const errParts = err.split("|");
+            error =
+                errParts.length > 1 ?
+                { message: errParts[1], debug: errParts[0] } :
+                { message: err };
+        } else {
+            error = {
+                message: err.message,
+                debug: JSON.stringify(err),
+            };
+        }
+        alert(error.message)
+        dispatch({ type: MSAL_FAILURE, payload: { error } });
     }
-    alert(error.message)
-    dispatch({ type: MSAL_FAILURE, payload: { error } });
-  }
 };
 
-export const logout = () => async (dispatch) => {
-  sessionStorage.clear();
-  dispatch(handleSetStep(0));
-  msalservice.logout();
+export const logout = () => async(dispatch) => {
+    sessionStorage.clear();
+    dispatch(handleSetStep(0));
+    msalservice.logout();
 };
 
-export const getAccount = () => async (dispatch) => {
-  const usermsal = msalservice.getAccount();
-  const step = usermsal === null ? 0 : 1;
-  dispatch(handleSetStep(step))
-  return usermsal;
+export const getAccount = () => async(dispatch) => {
+    const usermsal = msalservice.getAccount();
+    const step = usermsal === null ? 0 : 1;
+    dispatch(handleSetStep(step))
+    return usermsal;
 };
 
 export const getCenters = async(email, token) => {
-  return  await Axios.get(window.REACT_APP_CENTER_USER+'?email='+email, {
-    headers: {
-      "x-access-token": token
-    }
-  });
+    return await Axios.get(window.REACT_APP_CENTER_USER + '?email=' + email, {
+        headers: {
+            "x-access-token": token
+        }
+    });
 }
 
 const isNuevaAdmisionista = async(dispatch, email, getState) => {
     const result = await getCenters(email, getState().microsoftReducer.token);
-      
-    if(result.data.data.length > 0) {
-      dispatch(updateForm("centrosForm", result.data.data[0].centrosForm));
-      return 1
-    }else {
-      return 40
+
+    if (result.data.data.centrosForm) {
+
+        dispatch(updateForm("centrosForm", result.data.data.centrosForm));
+        return 1
+    } else {
+        return 40
     }
 };
 
-export const obtenerUsuarioSap = async (email, token) => {
-  return Axios.get(`${window.REACT_APP_HOMOLOGACION}?email=${email}&wt=${token.accessToken}`);
+export const obtenerUsuarioSap = async(email, token) => {
+    return Axios.get(`${window.REACT_APP_HOMOLOGACION}?email=${email}&wt=${token.accessToken}`);
 };
 
 export const guardar_token = (token) => {
-  return {
-    type: SAVE_TOKEN,
-    payload: token
-  }
+    return {
+        type: SAVE_TOKEN,
+        payload: token
+    }
 }
 
 const homologacionSap = async(dispatch, email, token) => {
-  const result = await obtenerUsuarioSap(email, token);
+    const result = await obtenerUsuarioSap(email, token);
 
-  if(result.data.content[0].length > 0 && result.data.token) { 
-      dispatch(guardar_token(result.data.token));
-      dispatch(updateForm("usuarioSAP", result.data.content[0]));
-    return true
-  }else {   
-    return false
-  }
+    if (result.data.content[0].length > 0 && result.data.token) {
+        dispatch(guardar_token(result.data.token));
+        dispatch(updateForm("usuarioSAP", result.data.content[0]));
+        return true
+    } else {
+        return false
+    }
 };
