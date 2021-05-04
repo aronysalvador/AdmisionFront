c6da25ae60c22b3af5a1f86b157399c923f66bea
Orianna Nuñez <onunez@achs.cl>
2021-05-04 14:50:10 -0400

    Cambios direccion de lugar de trabajo

--- src/css/comun.js
+++ src/css/comun.js
@@ -919,6 +919,11 @@ export const getComunStyle = makeStyles(() => ({
             verticalAlign: "top",
             minHeight: '73px' // '88px'
         },
+        textPrimaryDesk2: {
+            textAlign: "left",
+            verticalAlign: "top",
+            paddingBottom: "1em"
+        },
         imgPrimaryDesk: {
             float: 'right'
         },
